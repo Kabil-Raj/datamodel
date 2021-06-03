52e070943d5bb909e5491dedf265e1ddf138bc79
Kabil-Raj <raju.kabil@gmail.com>
2021-06-03 18:13:36 +0530

    commit

--- datamodel.go
+++ datamodel.go
@@ -1,7 +1,19 @@
 package datamodel
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func Sample() {
-	fmt.Println("hi")
+type ProductDetail struct {
+	ProductName        string    `json:"ProductName"`
+	ProductImageUrl    string    `json:"ProductImageUrl"`
+	ProductDescription string    `json:"ProductDescription"`
+	ProductPrice       string    `json:"ProductPrice"`
+	ProductReviews     string    `json:"ProductReviews"`
+	CreatedAt          time.Time `json:"CreatedAt"`
+}
+
+func Sample(productName string, productImageUrl string, productDescription string, productPrice string, productReviews string, createdTime time.Time) {
+	fmt.Println(" ", productName, " ", productImageUrl, " ", createdTime)
 }
