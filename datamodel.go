5b9a387fa0f095d4f7538d3289232bb09d520f0e
Kabil-Raj <raju.kabil@gmail.com>
2021-06-03 22:48:12 +0530

    commit

--- datamodel.go
+++ datamodel.go
@@ -19,23 +19,64 @@ type ProductDetail struct {
 func ConnectMySql() {
 	fmt.Println("Connecting MySql")
 
-	db, err := sql.Open("mysql", "root:Electronic1702!@tcp(127.0.0.1:3306)/test")
+	db, err := sql.Open("mysql", "root:Electronic1702!@tcp(127.0.0.1:3306)/")
 
 	if err != nil {
 		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Connected to Sql")
+	}
+
+	_, err = db.Exec("CREATE DATABASE AmazonProductDetailsDatabase")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(" Database created successfully ")
+	}
+
+	sqlStatement, err := db.Prepare("CREATE Table AmazonProductDetails(id int NOT NULL AUTO_INCREMENT, ProductName, ProductImageUrl, ProductDescription, ProductPrice, ProductReviews,CreatedTime,PRIMARY KEY (id));")
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	_, err = sqlStatement.Exec()
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Table created successfully  ")
 	}
 
 	defer db.Close()
 
-	insert, err := db.Query("INSERT INTO test VALUES ( 2, 'TEST' )")
+}
+
+func SaveData(productName string, productImageUrl string, productDescription string, productPrice string, productReviews string, createdTime time.Time) {
+	// Connecting to database
+	db, err := sql.Open("mysql", "root:Electronic1702!@tcp(127.0.0.1:3306)/")
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println("Connected to Sql")
+	}
+
+	// select database
+	_, err = db.Exec("USE AmazonProductDetailsDatabase")
 
 	if err != nil {
 		fmt.Println(err.Error())
+	} else {
+		fmt.Println(" testDB selected successfully ")
 	}
 
-	defer insert.Close()
-}
-
-func Sample(productName string, productImageUrl string, productDescription string, productPrice string, productReviews string, createdTime time.Time) {
-	fmt.Println(" ", productName, " ", productImageUrl, " ", createdTime)
+	// Insert values into database
+	sqlInsertStatement, err := db.Prepare("INSERT INTO employee (ProductName, ProductImageUrl, ProductDescription, ProductPrice, ProductReviews,CreatedTime) VALUES (productName,ProductImageUrl,productDescription,productPrice, productReviews,createdTime);")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	_, err = sqlInsertStatement.Exec()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
